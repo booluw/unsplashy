0e016a6662df3541229e4a79301dad4c5c5eb39e
= <=>
2022-01-15 13:13:11 +0100

    :tada: some axios configs

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = Axios
+const baseURL = 'https://api.unsplash.com/'
+const access_key = 'Ghoe8iCIVAxXYDXtg4YVIArtzqRRjr_kD7Y7vDHUUCo'
+
+Vue.prototype.$http.defaults.baseURL = baseURL
+Vue.prototype.$http.defaults.headers.common.Authorization = `Client-ID ${access_key}`
 
 new Vue({
   router,
